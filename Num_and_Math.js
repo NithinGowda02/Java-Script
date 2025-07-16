1463df678621082ff79324e7a08571d8dfe86c45
Nithin K P <nithinkp0212@gmail.com>
2025-07-16 14:01:04 +0530

    Number and Maths in Java-Script

---
+++ Num_and_Math.js
@@ -0,0 +1,20 @@
+const num1 = 100;
+const num2 = 75;
+
+let val;
+val = console.log(num1 + num2);
+val = console.log(num1 - num2);
+val = console.log(num1 * num2);
+val = console.log(num1 / num2);
+val = console.log(num1 % num2);
+
+console.log(Math.round(2.6765));
+console.log(Math.PI);
+console.log(Math.ceil(2.22));
+console.log(Math.floor(2.88));
+console.log(Math.sqrt(225));
+console.log(Math.abs(-67));
+console.log(Math.pow(8,3));
+console.log(Math.min(12,3,23,15,24,1,99));
+console.log(Math.max(12,3,23,15,24,1,99));
+console.log(Math.floor(Math.random()*1000 + 1));
