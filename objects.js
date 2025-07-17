bbc0728f0bc24b6874eee5d80782e4398f0d27a7
Nithin K P <nithinkp0212@gmail.com>
2025-07-17 13:19:03 +0530

    Arrays and objects in Java Script

---
+++ objects.js
@@ -0,0 +1,20 @@
+const personal_information = [
+    {
+    firstName: "Nithin",
+    Age:23,
+    },
+       {
+    firstName: "Madhan",
+    Age:21,
+    },
+       {
+    firstName: "Gagan",
+    Age:23,
+    },
+]
+console.log(personal_information[0].firstName)
+console.log(personal_information[0].Age)
+
+console.log(personal_information[2].firstName)
+console.log(personal_information[2].Age)
+
