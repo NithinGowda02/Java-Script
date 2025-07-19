74d4b74aaec53f7cd8e97c8e1fd2ee2a2bf2ac5d
Nithin K P <nithinkp0212@gmail.com>
2025-07-19 21:42:46 +0530

    Comparison Operator in JavaScript

---
+++ comparision.js
@@ -0,0 +1,11 @@
+const Age = parseInt(prompt("Enter your age:"));
+if (Age >= 18 && Age < 60) {
+  console.log("Adult");
+} else if (Age >=60){
+  console.log("Senior Citizen");
+}
+else {
+    console.log("Minor");
+    }
+
+
