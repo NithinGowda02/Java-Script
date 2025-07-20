fa53df1369313cc8526c286259d36a97beb8c94d
Nithin K P <nithinkp0212@gmail.com>
2025-07-20 14:44:06 +0530

    For Loop Example2 in Java Script

--- Loops/for_loop.js
+++ Loops/for_loop.js
@@ -1,7 +1,4 @@
-
-const table = prompt("enter the tables that you want >> ");
-const num = 10;
-
-for(let i=1;i<=num;i++){
-        console.log(table +"x"+i+"="+table*i)
+names = ["Alice", "Bob", "Charlie", "David", "Eva"];
+for (let i = 0; i < names.length; i++) {
+    console.log("Hello " + names[i]);
 }
