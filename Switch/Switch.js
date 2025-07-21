47e18975a035020778c9361c92731407ba7ad180
Nithin K P <nithinkp0212@gmail.com>
2025-07-21 22:58:46 +0530

    Switch Case in Java Script

---
+++ Switch/Switch.js
@@ -0,0 +1,11 @@
+const day = "Monday";
+switch (day) {
+    case "Monday":
+        console.log("Start of the work week");
+        break;
+    case "Friday":
+        console.log("End of the work week");
+        break;
+    default:
+        console.log("Midweek days are busy");
+}
