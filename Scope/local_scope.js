dbe60ebc272c2e3733caf80e56584ddbd49cf329
Nithin K P <nithinkp0212@gmail.com>
2025-07-24 09:55:29 +0530

    local scope in Java Script

---
+++ Scope/local_scope.js
@@ -0,0 +1,4 @@
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+//console.log(i); -->here i is calling outside the block of for loop since it is declared inside the for it has the accessebility only inside the for loop//
