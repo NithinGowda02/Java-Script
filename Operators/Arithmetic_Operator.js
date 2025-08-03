4d1bb6c5a3f0b93843f5bde713ebd8c6e9183e4a
Nithin K P <nithinkp0212@gmail.com>
2025-08-03 21:34:15 +0530

    Arithmetic Operators

---
+++ Operators/Arithmetic_Operator.js
@@ -0,0 +1,10 @@
+let num1 = 18;
+let num2 = 2;
+
+console.log(`Number1 : ${num1} & Number2 : ${num2}`);
+console.log(`Number1 + Number2 >> ${num1 + num2}`);
+console.log(`Number1 - Number2 >> ${num1 - num2}`);
+console.log(`Number1 * Number2 >> ${num1 * num2}`);
+console.log(`Number1 / Number2 >> ${num1 / num2}`);
+console.log(`Number1 ** Number2 >> ${num1 ** num2}`);
+console.log(`Number1 % Number2 >> ${num1 % num2}`);
