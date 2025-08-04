f445c28c4714b080e394de688fab1000abd2b277
Nithin K P <nithinkp0212@gmail.com>
2025-08-04 21:46:34 +0530

    Added Logical Operator

---
+++ Operators/Logical_Operator.js
@@ -0,0 +1,6 @@
+num1 = 12;
+num2 = 23;
+console.log(`Number1 : ${num1} & Number2 : ${num2}`);
+console.log(`Number1 && Number2 >> ${num1 && num2}`);
+console.log(`Number1 || Number2 >> ${num1 || num2}`);
+console.log(`!Number1 >> ${!num1}`);
