00a92a691689ce138b0373b1ca2a0fe0a0cee0c9
Nithin K P <nithinkp0212@gmail.com>
2025-08-04 22:36:40 +0530

    Added Ternary Operator

---
+++ Conditional_Statements/Ternary_oprator.js
@@ -0,0 +1,4 @@
+let age = prompt("Enter your Age >> ");
+console.log(age >= 18 ? console.log("You are adult"): console.log("you're Not adult"));
+// The above code uses a ternary operator to check if the user is eligible to vote based on their age.
+// If the age is 18 or more, it prints "You are eligible to vote."
