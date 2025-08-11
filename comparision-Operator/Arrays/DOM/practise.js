2feb2927b0c7693cd0fe8e1e01cc083b0db15a61
Nithin K P <nithinkp0212@gmail.com>
2025-08-11 22:42:28 +0530

    added prcatise question on DOM

---
+++ comparision-Operator/Arrays/DOM/practise.js
@@ -0,0 +1,6 @@
+let button1 = document.createElement("button");
+button1.innerText="Click Me!";
+button1.style.backgroundColor= "#ff0000";
+button1.style.color= "#ffffff";
+document.querySelector("body").prepend(button1);
+
