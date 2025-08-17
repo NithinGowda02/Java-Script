99c6d01acc3c1f789f88555ac05b7788bb723a85
Nithin K P <nithinkp0212@gmail.com>
2025-08-17 22:23:16 +0530

    Added Classes and Objects constructor

---
+++ Event-Handling/Objects/Classes And Objects/constructor.js
@@ -0,0 +1,13 @@
+class ToyotaCar {
+    constructor(brand, model, price){
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+    DisplayInfo() {
+        console.log(`Toyota is launching new Brand ${this.brand} of model ${this.model} with price ${this.price}.`)
+    }
+}
+
+ fortuner = new ToyotaCar("Fortuner", "2025", "500000");
+ fortuner.DisplayInfo()
