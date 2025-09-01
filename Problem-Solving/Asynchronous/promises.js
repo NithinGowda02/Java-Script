6b0f8ccce6750d5d17178cd4e7c5bdc92dc74ccc
Nithin K P <nithinkp0212@gmail.com>
2025-09-01 22:10:28 +0530

    Added Promise

---
+++ Problem-Solving/Asynchronous/promises.js
@@ -0,0 +1,17 @@
+const getData = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data Fetching....")
+            resolve("success");
+        },3000);
+        
+    });
+    
+}
+getData().then((res) => {
+    return getData()
+    }).then((res) => {
+        return getData()
+    }).then((res) => {
+        console.log(res);
+    })
